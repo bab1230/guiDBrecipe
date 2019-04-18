d59515f2dc09a0124ed7ff0897beda52b799a63b
Franco Paz <fpaz@smu.edu>
2019-04-18 17:54:50 -0500

    Favorites Page and API

---
+++ src/components/Favorites/Favorite.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FavoriteList } from './FavoriteList';
+import { FavoriteRepository } from '../../api';
+import { Link } from 'react-router-dom';
+import './Favorite.css';
+
+export class Favorite extends React.Component{
+    FavoriteRepository = new FavoriteRepository;
+
+    state={
+        favorites: []
+    };
+
+    onDelete(favoriteId){
+        if(window.confirm("Are you sure?")){
+            this.FavoriteRepository.deleteFavorite(favoriteId)
+                .then(() => {
+                    this.setState(state => ({
+                        favorites: state.favorites.filter(x => x.id !== favoriteId)
+                    }))
+                });
+        }
+    }
+    render(){
+        return(
+            <>
+                <Link to="/new" className="btn btn-success float-right">
+                    <i className="fa fa-edit"></i>
+                    Add to Favorites
+                </Link>
+                {!!this.state.favorites.length &&
+                <FavoriteList favorites={this.state.favorites} onDelete={x => this.onDelete(x)}/>}
+            </>
+        )
+    }
+
+    componentDidMount(){
+        this.FavoriteRepository.getFavorites()
+        .then(favorites => this.setState({favorites}));
+    }
+}
+
+export default Favorite;
