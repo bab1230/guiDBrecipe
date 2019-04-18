3f63f5b5310514cbbde6175e41762b54d2806619
skylert7 <skyler.linhtran@gmail.com>
2019-04-18 12:18:37 -0500

    favorites endpoint

--- server/server.js
+++ server/server.js
@@ -1,47 +1,10 @@
-// const Recipe = require("./recipeProfile")
-// var express = require('express');
-// var path = require('path');
-// var bodyParser = require('body-parser');
-// var session = require('express-session');
-// var app = express();
-// const port = process.env.PORT || 4000;
-//
-// //app.use(cookieParser());
-// app.use(bodyParser.json());
-// //app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: false }));
-//
-// //More to come
-// app.get('/recipe', (req, res) => {
-// 		Recipe.findOne({
-// 				where: {
-// 						recipe_id: 3
-// 				}
-// 		})
-// 				.then(recipeData => {
-// 					//get cooking instruction from recipes table
-// 					res.send(recipeData.how_to_cook)
-//
-// 		}).catch(err => {
-// 				res.send('error: ' + err)
-// 		})
-// })
-//
-// app.listen(port, () => {
-//   console.log("Server is running on port: " + port)
-// })
-
-
 var express = require('express');
 var path = require('path');
-var cors = require("cors")
+var mysql = require('mysql');
 const jwt = require("jsonwebtoken")
 const cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-const Sequelize = require("sequelize")
-const db = require("./db")
-// var Users = require('./routes/users');
 var app = express();
 
 process.env.SECRET_KEY = 'secret'
@@ -51,8 +14,14 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true,
+	cookie  : { maxAge  :  86400 * 1000)}
+}));
 
-process.env.SECRET_KEY = 'secret'
+//------------------------------------ Connection to MySQL ------------------------------------
 
 var connection = mysql.createPool({
     connectionLimit : process.env.mysql_connection_pool_Limit, // default:10
@@ -63,35 +32,63 @@ var connection = mysql.createPool({
     database: 'dbteam'
 });
 
+//------------------------------------ Register ------------------------------------
 app.post('/users/register', (req, res) => {
-    const userData = [req.body.user_id, req.body.first_name, req.body.last_name, req.body.user_name, req.body.user_password]
+    const userData = [req.body.user_id,
+                    req.body.first_name,
+                    req.body.last_name,
+                    req.body.user_name,
+                    req.body.user_password]
 
-    connection.query('INSERT INTO users (user_id, first_name, last_name, user_name, user_password) VALUES (?, ?, ?, ?, ?)', userData, function(err, result) {
-      if (err) throw err
-      else{
-        console.log("user ", user_name, "created")
-      }
-  })
+    connection.query('INSERT INTO users (user_id, first_name, last_name, user_name, user_password) VALUES (?, ?, ?, ?, ?)',
+                    userData, function(err, result) {
+                  if (err) throw err
+                  else{
+                    console.log("user ", req.body.user_name, "created")
+                  }
+                })
 })
 
+//------------------------------------ Login ------------------------------------
 app.post('/users/login', (req, res) => {
     var user_name = req.body.user_name
     var user_password = req.body.user_password
     if (user_name && user_password) {
-            connection.query('SELECT * FROM users WHERE user_name = ? AND user_password = ?', [user_name, user_password], function(error, results, fields) {
-                      if (results.length > 0) {
-                          request.session.loggedin = true;
-                          request.session.username = username;
-                          response.redirect('/home');
+            connection.query('SELECT * FROM users WHERE user_name = ? AND user_password = ?',
+                            [user_name, user_password], function(error, results, fields) {
+                      if (results.length === 1) {
+                          console.log("Login Success!")
                         } else {
-                          response.send('Incorrect Username and/or Password!');
+                          res.send('Incorrect Username and/or Password!');
                         }
-                      response.end();
+                      res.end();
                       });
     } else {
-        response.send('Please enter Username and Password!');
-        response.end();
+        res.send('Please enter Username and Password!');
+        res.end();
     }
+})
+
+//------------------------------------ Logout ------------------------------------
+
+app.get('/users/logout', function (req, res) {
+  if (req.session.loggedin){
+    req.session.destroy();
+    res.send("Logout successful!");
+  }
+  else{
+    res.send("<h1>You are not logged in. Can't log out!</h1>")
+  }
+});
+
+
+//------------------------------------ User Favorite ------------------------------------
+app.get('/users/favorite', (req, res) => {
+  connection.query('SELECT * FROM users NATURAL JOIN favorites ON users.user_id = favorites.user_id WHERE user_id = ?',
+                    [req.body.user_id], function(error, results, fields) {
+            res.send(results);
+            res.end();
+            });
 })
 
 
