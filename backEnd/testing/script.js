f3d4c8a469d9dc50ebbd4d80f29273f0e8df1d99
Emily Fashenpour <emilyfashenpour@Emilys-MacBook-Pro-4.local>
2019-04-22 22:52:31 -0500

    added more api calls

--- backEnd/testing/script.js
+++ backEnd/testing/script.js
@@ -38,4 +38,73 @@ app.get('/jkjk',function(req,res){
   });
 })
 
+app.get('/favorites', function(req, res){
+  var user_id = 53;
+  connection.query('SELECT r.recipe_id, recipe_name, cuisine_type, how_to_cook, image FROM favorites f JOIN recipes r ON r.recipe_id = f.recipe_id WHERE user_id = ' + user_id, function(error, rows, fields){
+    if (!!error){
+        console.log('Error with GET favorties query!')
+    } else {
+        console.log('GET favorites query success!');
+        res.send(rows)
+    }
+  })
+})
+
+app.post('/favorites', function(req, res){
+  var user_id = 53;
+  var recipe_id = 5;
+  connection.query('INSERT INTO favorites VALUES(' + user_id + ',' + recipe_id + ')', function(error, rows, fields){
+    if (!!error){
+        console.log('Error with POST favorites query!')
+    } else {
+        console.log('POST favorites query success!');
+        res.send(rows)
+    }
+  })
+
+})
+
+app.get('/ingredients', function(req, res){
+  var user_id = 53;
+  connection.query('SELECT ingredient_id, ingredient_name, quantity, unit FROM inventory i JOIN ingredient_all a ON i.ingredient_id = a.ingredient_id WHERE i.user_id =' + user_id, function(error, rows, fields){
+    if (!!error){
+        console.log('Error with GET ingredients query!')
+    } else {
+        console.log('GET ingredients query success!');
+        res.send(rows)
+    }
+  })
+})
+
+app.get('/cookware', function(req, res){
+  var user_id = 53;
+  connection.query('SELECT cookware_id, cookware_name FROM cookware WHERE user_id =' + user_id, function(error, rows, fields){
+    if (!!error){
+        console.log('Error with GET cookware query!')
+    } else {
+        console.log('GET cookware query success!');
+        res.send(rows)
+    }
+  })
+})
+
+app.get('/trending', (req,res) => {
+  var today = new Date().toJSON().slice(0,10);
+  console.log(today);
+//   connection.query('SELECT * FROM recipes WHERE featured_date = ' + today, function(error, results, fields) {
+//   if (!!error){
+//       console.log('Error with GET trending query!')
+//   } else {
+//       console.log('GET trending query success!');
+//       res.send(rows)
+//   }
+// });
+})
+
+
+//search by recipe_name
+
+//search by recipe/cuisine_type
+
+
 app.listen(5000);
