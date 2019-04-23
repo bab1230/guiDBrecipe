fdb43597afb0a245be8952339c3f473954f30299
Franco Paz <fpaz@smu.edu>
2019-04-22 22:53:33 -0500

    popover update

--- src/components/Favorites/FavoriteList.jsx
+++ src/components/Favorites/FavoriteList.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Popover, Card, OverlayTrigger } from 'react-bootstrap';
 import { Link } from 'react-bootstrap';
 
+const popover=(
+    <Popover id="popover-basic">
+        <Card>
+            <Card.Img className="rounded" variant="top" src='http://johnlawrimore.com/smu/101.png' />
+            <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+    </Popover>
+);
 export const FavoriteList = (props) => (
     <Table striped bordered hover size="sm">
         <thead>
@@ -15,7 +32,11 @@ export const FavoriteList = (props) => (
                 props.favorites.map((a, i) =>
                     <tr key={i}>
                         <td>
-                            <Link to={`/edit/${a.id}`}>{a.name}</Link>
+                            <Link to={`/edit/${a.id}`}>
+                                <OverlayTrigger trigger="hover" placement="right" overlay={popover}>
+                                    {a.name}
+                                </OverlayTrigger>
+                            </Link>
                         </td>
                         <td>{ a.type }</td>
                     </tr>
