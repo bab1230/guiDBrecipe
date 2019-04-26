3dca723b2bb0c8b46aa6635f96e7054ca5cd4f47
Shadowninja33 <prestondcobb@gmail.com>
2019-04-25 19:57:02 -0500

    added getIngredients skeleton to ingredients api

--- src/api/ingredientsRepo.js
+++ src/api/ingredientsRepo.js
@@ -2,15 +2,14 @@ import React from 'react';
 
 class IngredientsRepo extends React.Component {
 
-
+  url = "http://ec2-18-222-255-36.us-east-2.compute.amazonaws.com:4000";
     
-  render() {
-    return <p>{this.state.someKey}</p>;
-  }
-
-
-  componentDidMount() {
-    this.setState({ someKey: 'otherValue' });
+  getIngredients() {
+    return new Promise((resolve, reject) => {
+      axios.get(`${this.url}/users/pantry`)
+          .then(resp => resolve(resp.data))
+          .catch(resp => alert(resp));
+  });
   }
 }
 
