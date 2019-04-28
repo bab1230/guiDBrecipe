153d927240f509d7bcaf54bd747a096f119a56fe
skylert7 <skyler.linhtran@gmail.com>
2019-04-28 16:56:03 -0500

    test

--- server/server.js
+++ server/server.js
@@ -210,8 +210,8 @@ app.post('/users/favorite/add', (req, res) => {
 app.get('/users/pantry', (req, res) => {
 
 		var inStock = {}
-					connection.query('SELECT i.ingredient_id, ingredient_name, amount, unit FROM inventory i JOIN ingredient_all a ON i.ingredient_id = a.ingredient_id WHERE i.user_id = ' + parseInt(req.body.user_id, 10),
-											 function(error, results, fields) {
+					connection.query('SELECT i.ingredient_id, ingredient_name, amount, unit FROM inventory i JOIN ingredient_all a ON i.ingredient_id = a.ingredient_id WHERE i.user_id = ?' ,
+					[parseInt(req.body.user_id, 10)], function(error, results, fields) {
 														if(error) {
 															console.log('Error with GET ingredients query!');
 															throw error
