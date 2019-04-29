37d54be0e909fe129d30429a70b8da24c75a52b4
Braden Wallace <bradenwallace@Bradens-MacBook-Pro.local>
2019-04-28 22:29:53 -0500

    navbar is responsive and account basically works

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -12,20 +12,23 @@ class Navigation extends Component {
     render() {
         return (
             <>
-                <Navbar bg="dark" variant="dark" style={{ margin: 20 }}>
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{ margin: 20 }}>
                     <Navbar.Brand><h3>Recipeazy</h3></Navbar.Brand>
-                    <Nav className="mr-auto">
-                        <NavDropdown className="flow-right" id="dropdown-menu-align-right" title="Account">
-                            <NavDropdown.Item onClick={() => this.toggleAccount()}>Account</NavDropdown.Item>
-                            <NavDropdown.Item>My Ingredients</NavDropdown.Item>
-                            <NavDropdown.Item>My Favorites</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item>Logout</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Ingredients</Nav.Link>
-                        <Nav.Link as={NavLink} to="/favorite">Favorites</Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
+                            <NavDropdown className="flow-right" id="dropdown-menu-align-right" title="Account">
+                                <NavDropdown.Item onClick={() => this.toggleAccount()}>Account</NavDropdown.Item>
+                                <NavDropdown.Item>My Ingredients</NavDropdown.Item>
+                                <NavDropdown.Item>My Favorites</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item>Logout</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">Ingredients</Nav.Link>
+                            <Nav.Link as={NavLink} to="/favorite">Favorites</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                     <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                         <Button variant="outline-info">Search</Button>
