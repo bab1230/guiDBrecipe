077ac4de7a2a872b4add51aeba3216592234adb1
Shadowninja33 <prestondcobb@gmail.com>
2019-04-30 00:40:50 -0500

    fixed route, fixed api call for product details

--- src/api/recipeRepo.js
+++ src/api/recipeRepo.js
@@ -1,23 +1,34 @@
 import axios from 'axios';
 
 export class recipeRepo {
-    url = "http://ec2-18-222-255-36.us-east-2.compute.amazonaws.com:4000/all_recipes";
+    url = "http://ec2-18-222-255-36.us-east-2.compute.amazonaws.com:4000";
 
     getRecipes() {
         return new Promise((resolve, reject) => {
-            axios.get(this.url, this.config)
+            axios.get(`${this.url}/all_recipes`, this.config)
                 .then(resp => resolve(resp.data))
                 .catch(resp => alert(resp));
         });
     }
 
-    getRecipe(recipe_id) {
+
+    getRecipe(id) {
         return new Promise((resolve, reject) => {
-            axios.get(`${this.url}/${recipe_id}`, {params: { recipe_id: 1 }})
+            axios.get(`${this.url}/recipe/`, {params: { recipe_id: id }})
                 .then(resp => resolve(resp.data))
                 .catch(resp => alert(resp));
         });
     }
+
+
+    getRecipeIngredients(id) {
+        return new Promise((resolve, reject) => {
+            axios.get(`${this.url}/recipe/ingredients`, {params: { recipe_id: id }})
+                .then(resp => resolve(resp.data))
+                .catch(resp => alert(resp));
+        });
+    }
+
 
     addReview(recipe_id, review) {
         return new Promise((resolve, reject) => {
