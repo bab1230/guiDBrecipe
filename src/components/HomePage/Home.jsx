352c9093c36a4a8d1de923957ca88addd9fda66d
Braden Wallace <bradenwallace@Bradens-MacBook-Pro.local>
2019-04-30 22:14:31 -0500

    home page is responsive

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -34,44 +34,59 @@ export class Home extends React.Component {
             <div className="d-flex h-50 flex-column align-items-center">
                 <h4 style={{ margin: 20 }}>Trending</h4>
                 <div className="d-flex" style={{}} >
-                    <Card className="offset-1 col-3" style={{maxHeight: "700px"}}>
-                        <Card.Img className="rounded" variant="top" src='http://johnlawrimore.com/smu/101.png' />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
+                    <div className="hide-card offset-1 col-3">
+                        <Card style={{maxHeight: "700px"}}>
+                            <Card.Img className="rounded" variant="top" src='http://johnlawrimore.com/smu/101.png' />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                        This is a wider card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer.
                                     </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Carousel className="col-4 align-items-center" style={{ maxHeight: "700px", maxWidth: "700px"}}>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col-xl-4 col-md-4 col-xs-12 align-items-center">
+                    <Carousel className="phone-trending fix-carousel" style={{ maxHeight: "700px", maxWidth: "700px"}}>
                     {
                         this.state.recipes.map((a, i) => (
                             <Carousel.Item key={i}>
                                 <Link to={`/recipes/${a.recipe_id}`}  >
-                                <img 
-                                    className="rounded trim d-block w-100"
-                                    src={a.image}
-                                    />
-                                <Carousel.Caption>
-                                    <h3 style={{color: "white", fontWeight: "bold", textShadow: "black 0px 0px 10px"}}>{a.recipe_name}</h3>
-                                </Carousel.Caption>
+                                    <Card className="border-0 ">
+                                        <Card.Img src={a.image}/>
+                                    </Card>
+                                    <Carousel.Caption>
+                                        <h3 style={{color: "white", fontWeight: "bold", textShadow: "black 0px 0px 10px"}}>{a.recipe_name}</h3>
+                                    </Carousel.Caption>
                                 </Link>
                             </Carousel.Item>
+                            /*<Carousel.Item key={i}>
+                                <Link to={`/recipes/${a.recipe_id}`}  >
+                                    <img 
+                                        className="rounded trim d-block"
+                                        src={a.image}
+                                        />
+                                    <Carousel.Caption>
+                                        <h3 style={{color: "white", fontWeight: "bold", textShadow: "black 0px 0px 10px"}}>{a.recipe_name}</h3>
+                                    </Carousel.Caption>
+                                </Link>
+                            </Carousel.Item>*/
                         ))
                     }
                     </Carousel>
-
-                    <Card className="col-3" >
-                        <Card.Img className="rounded" variant="top" src='http://johnlawrimore.com/smu/101.png' />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
+                    </div>
+                    <div className="hide-card col-3">
+                        <Card className="" >
+                            <Card.Img className="rounded" variant="top" src='http://johnlawrimore.com/smu/101.png' />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                        This is a wider card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer.
                                     </Card.Text>
-                        </Card.Body>
-                    </Card>
+                            </Card.Body>
+                        </Card>
+                    </div>
                 </div>
             </div>
         );
